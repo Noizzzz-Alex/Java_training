da094a94f66968e2b950c3d6c761c52092d58b23
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-03-24 23:40:41 +0700

    added task#1 of homework#1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+
+
 public class Main {
+    /*
+    n треугольное и !n
+     */
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello everyone");
+        int n = 10;
+        int tre = 0;
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            tre = tre + i;
+            fact = fact * i;
+        }
+        System.out.println(tre);
+        System.out.println(fact);
     }
+
+
 }
