6ecc565b642334aadacafaf17ad25dd1889b5e7e
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-03-26 17:51:19 +0700

    added task#2 of homework#1

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,26 @@
 
 public class Main {
     /*
-    n треугольное и !n
+    Вывести все простые числа от 1 до 1000
      */
 
     public static void main(String[] args) {
-        int n = 10;
-        int tre = 0;
-        int fact = 1;
+        int n = 1000;
+
+
         for (int i = 1; i <= n; i++) {
-            tre = tre + i;
-            fact = fact * i;
+            int counter = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i % j == 0) {
+                    counter++;
+                }
+            }
+            if (counter == 2) {
+                System.out.printf("%d, ", i);
+            }
+
+
         }
-        System.out.println(tre);
-        System.out.println(fact);
     }
 
 
