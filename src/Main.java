6517b698c6e7f2d4ef0f7b6510300821ebeaa022
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-03-27 05:02:12 +0700

    init task#3 of HomeWork#1

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,17 @@
 
 public class Main {
     /*
-    Вывести все простые числа от 1 до 1000
+    Реализовать простой калькулятор (+ - / *)
+    Ввод числа ->
+    Ввод знака ->
+    Ввод числа ->
      */
 
     public static void main(String[] args) {
-        int n = 1000;
 
 
-        for (int i = 1; i <= n; i++) {
-            int counter = 0;
-            for (int j = 1; j <= n; j++) {
-                if (i % j == 0) {
-                    counter++;
-                }
-            }
-            if (counter == 2) {
-                System.out.printf("%d, ", i);
-            }
-
-
-        }
     }
+}
 
 
 }
