6f949559b3465936755513ac0423d1a03831d393
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-03-27 23:12:07 +0700

    added solution task#3 of HomeWork#1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class Main {
     /*
@@ -9,10 +9,40 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        System.out.println("Введите первое число: ");
+        Scanner sc_number_first = new Scanner(System.in);
+        //sc_number_first.close();
+        int first_number = Integer.parseInt(sc_number_first.nextLine());
 
+        System.out.println("Действие(+, -, *, /): ");
+        Scanner act = new Scanner(System.in);
+        //act.close();
+        char action = act.next().charAt(0);
 
+        System.out.println("Введите второе число: ");
+        Scanner sc_number_two = new Scanner(System.in);
+        //sc_number_two.close();
+        int number_two = Integer.parseInt(sc_number_two.nextLine());
+
+        int result;
+        if (action == '+') {
+            result = first_number + number_two;
+            System.out.printf("Результат = %d ", result);
+        }
+        if (action == '-') {
+            result = first_number - number_two;
+            System.out.printf("Результат = %d ", result);
+        }
+        if (action == '*') {
+            result = first_number * number_two;
+            System.out.printf("Результат = %d ", result);
+        }
+        if (action == '/') {
+            result = first_number / number_two;
+            System.out.printf("Результат = %d ", result);
+        }
     }
 }
 
 
-}
+
