8490013ef40da3278dc9489e39e775910238c57f
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-03-31 01:41:23 +0700

    added class Sorting_Bubble

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,29 @@
 результат после каждой итерации запишите в лог-файл.
  */
 
+import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        int[] array_n = new int[]{5, 12, 57, 37, 56, 65, 684, 1869, 17};
-        int k = array_n.length;
-
-        for (int i = 0; i < k - 1; i++) {
-            if (array_n[i] > array_n[i +1]){
-                int temp = array_n[i];
-                array_n[i] = array_n[i+1];
-                array_n[i+1] = temp;
-                i = 0;
-            }
-
-
-
-        }
-        IntStream.range(0, k).forEach(i -> System.out.printf("%d, ", array_n[i]));
+        int[] array_n = new int[]{5, 120, 57, 307, 56, 65, 684, 1869, 17};
+//        int k = array_n.length;
+//        Logger log = Logger.getLogger("");
+//        for (int i = 0; i < k - 1; i++) {
+//            if (array_n[i] > array_n[i +1]){
+//                int temp = array_n[i];
+//                array_n[i] = array_n[i+1];
+//                array_n[i+1] = temp;
+//                i = 0;
+//            }
+//
+//
+//
+//        }
+        Sorting_Bubble.Bubble(array_n);
+        IntStream.range(0, array_n.length).forEach(i -> System.out.printf("%d, ", array_n[i]));
 
 
 
