e2d556b12c8b41b5ca4b03e9915567d82d13aab5
Noizzzz-Alex <118923841+Noizzzz-Alex@users.noreply.github.com>
2023-04-01 23:30:11 +0700

    added Logger

--- src/Sorting_Bubble.java
+++ src/Sorting_Bubble.java
@@ -1,15 +1,20 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Sorting_Bubble {
-    public static void Bubble(int[]array){
+    public static void Bubble(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i +1]){
+            if (array[i] > array[i + 1]) {
+
                 int temp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = temp;
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
                 i = 0;
             }
 
 
-
         }
     }
+
+
 }
